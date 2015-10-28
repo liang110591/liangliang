ce28d4c63ca31f455e9d12fec6169f922b353ea7
T mac <t_mac@t-macdeimac.local>
2015-10-28 11:44:51 +0800

    测试

---
+++ gittest11/gittest11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest11
+//
+//  Created by T_mac on 15/10/28.
+//  Copyright © 2015年 T_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest11/gittest11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest11
+//
+//  Created by T_mac on 15/10/28.
+//  Copyright © 2015年 T_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
